d9b52837f396125dbffb6c33fd0602ae23427304
hunterdyar <bloop@hdyar.com>
2023-10-21 00:21:01 -0400

    Sliced Terrain Placeholder

---
+++ Assets/Scripts/MapGen/SlicedTerrain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MapGen
+{
+	[CreateAssetMenu(fileName = "Terrain", menuName = "Golf/Sliced Terrain Definition", order = 0)]
+	public class SlicedTerrain : ScriptableObject
+	{
+		[SerializeField] private GameObject[,] _map;
+
+		
+		//what's the input here...
+		public GameObject GetTile()
+		{
+			return null;
+		}
+	}
+}
